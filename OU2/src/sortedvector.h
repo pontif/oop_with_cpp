9535c4cb95cf25429f9c58cbb10e111f286968bd
eponfer <pontus.fernstrom@ericsson.com>
2016-03-11 22:23:06 +0100

    All fixed, but still leaking memory. I think.

--- OU2/src/sortedvector.h
+++ OU2/src/sortedvector.h
@@ -8,47 +8,56 @@ using namespace std;
 template <class T, int sz> class SortedVector
 {
 public:
-  SortedVector();
-  ~SortedVector();
-  bool add(const T& v);
-  T& median();
+  // Constructor (trivial destructor)
+  SortedVector() : size(sz), nOccupied(0){}
+
+  // Information getters
+  T& median() {return vec[nOccupied / 2];}
+
+  // Vector modifiers
+  bool add(const T& v); // Returns false if vector is full
   void removeLarger(const T& v);
+
+  // Output operator, returning string with one element per row
   void print(ostream &os);
 private:
+  // Static size of vector
   int size;
+  // Current number of members
   int nOccupied;
+  // Template vector
   T vec[sz];
 };
 
-template<class T, int sz> SortedVector<T, sz>::SortedVector() : 
-  size(sz), nOccupied(0) {}
-
-template<class T, int sz> SortedVector<T, sz>::~SortedVector() {}
-
 template<class T, int sz> bool SortedVector<T, sz>::add(const T &v)
 {
-//  cout << "\nnOccupied: " << nOccupied << "\n";
-//  if (nOccupied == size) { cout << "\nFull\n"; return false; }
+  if (nOccupied == size) { return false; }
+
+  // Find first index with larger element
   int i = 0;
   while ((i < nOccupied) && (v >= vec[i])) { i++; }
+
+  // Move all later elements one step back to make place
   int j = ++nOccupied;
   while (j > i) { vec[j] = vec[j - 1]; j--; }
-  vec[i] = v;
-//  print(cout);
-  return true;
-}
 
-template<class T, int sz> T& SortedVector<T, sz>::median()
-{
-  return vec[nOccupied / 2];
+  // Add new element
+  vec[i] = v;
+  return true;
 }
 
 template<class T, int sz> void SortedVector<T, sz>::removeLarger(const T &v)
 {
+  // Find first index with larger element
   int i = 0;
   while ((i < nOccupied) && (v >= vec[i])) { i++; }
+
+  // Save new length
   int newLength = i;
-  while (i < nOccupied) {i++;}
+  /* TODO:PF How to delete properly? */
+  while (i < nOccupied) {/*delete &vec[i]*/; i++;}
+
+  // Update length
   nOccupied = newLength;
 }
 
